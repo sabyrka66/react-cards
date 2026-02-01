9aa19fad28fe1e78a8180d185b619986759e5261
Sabyrka <sabyrka66@gmail.com>
2026-02-01 22:58:46 +0500

    feat: implement footer

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -6,7 +6,10 @@ export const MainLayout = () => {
       <header>Header</header>
       <div className={styles.mainWrapper}>
         <main className={styles.main}>Main</main>
-        <footer className={styles.footer}>Footer</footer>
+        <footer className={styles.footer}>
+          React Question Cards Application | {new Date().getFullYear()} <br />
+          by Sabyrka
+        </footer>
       </div>
     </div>
   )
