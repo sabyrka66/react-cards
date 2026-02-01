398456b39de9aef9b2e6776954115e527ec07448
Sabyrka <sabyrka66@gmail.com>
2026-02-01 23:10:34 +0500

    feat: integrate routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MainLayout } from './components/MainLayout'
 
 export const App = () => {
-  return <MainLayout />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element="main" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import styles from './MainLayout.module.css'
 
 export const MainLayout = () => {
@@ -5,7 +6,9 @@ export const MainLayout = () => {
     <div className={styles.mainLayout}>
       <header>Header</header>
       <div className={styles.mainWrapper}>
-        <main className={styles.main}>Main</main>
+        <main className={styles.main}>
+          <Outlet />
+        </main>
         <footer className={styles.footer}>
           React Question Cards Application | {new Date().getFullYear()} <br />
           by Sabyrka
