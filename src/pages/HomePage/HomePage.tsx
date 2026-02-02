1567e4b91d3b77e9438cecede40091ded7f117b8
Sabyrka <sabyrka66@gmail.com>
2026-02-02 20:25:01 +0500

    feat: artificial delay

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,29 @@
 import { Loader } from '@/components/Loader'
 import { QuestionCardList } from '@/components/QuestionCardList'
 import { API_URL } from '@/constants'
+import { artificialDelay } from '@/heplers/artificialDelay'
 import type { QuestionApiItem } from '@/types'
 import { useEffect, useState } from 'react'
 
 export const HomePage = () => {
   const [questionCards, setQuestionCards] = useState<QuestionApiItem[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const getQuestions = async () => {
       try {
+        setIsLoading(true)
+
+        await artificialDelay()
+
         const response = await fetch(`${API_URL}/react`)
         const questions = await response.json()
+
         setQuestionCards(questions)
       } catch (error) {
         console.log(error)
+      } finally {
+        setIsLoading(false)
       }
     }
 
@@ -23,7 +32,7 @@ export const HomePage = () => {
 
   return (
     <div>
-      <Loader />
+      {isLoading && <Loader />}
       <QuestionCardList items={questionCards} />
     </div>
   )
