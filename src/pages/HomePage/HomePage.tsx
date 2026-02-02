bfe64eda54d339019b720f9b78d76ac4c0a5e761
Sabyrka <sabyrka66@gmail.com>
2026-02-02 21:00:15 +0500

    feat: implement changeable input

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,10 +3,11 @@ import { QuestionCardList } from '@/components/QuestionCardList'
 import { API_URL } from '@/constants'
 import { useFetch } from '@/hooks/useFetch'
 import type { QuestionApiItem } from '@/types'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type ChangeEvent } from 'react'
 
 export const HomePage = () => {
   const [questionCards, setQuestionCards] = useState<QuestionApiItem[]>([])
+  const [searchValue, setSearchValue] = useState<string>('')
 
   const { fetchResponse, isLoading, error } = useFetch(async () => {
     const response = await fetch(`${API_URL}/react`)
@@ -20,8 +21,15 @@ export const HomePage = () => {
     fetchResponse('react')
   }, [])
 
+  const searchValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
+    console.log(event.target.value)
+  }
+
   return (
     <div>
+      <input type="text" value={searchValue} onChange={searchValueHandler} />
+
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
       <QuestionCardList items={questionCards} />
