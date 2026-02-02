484baaea787d7b4b2f037172729c3c8df13e2069
Sabyrka <sabyrka66@gmail.com>
2026-02-02 16:59:59 +0500

    feat: implement question card

--- src/components/QuestionCard/QuestionCard.tsx
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -1,3 +1,25 @@
+import styles from './QuestionCard.module.css'
+import { Button } from '../Button'
+
 export const QuestionCard = () => {
-  return <div>QuestionCard</div>
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <span>Level: 1</span>
+        <div>Not Completed</div>
+      </div>
+
+      <p className={styles.cardTitle}>Lorem ipsum dolor sit amet?</p>
+
+      <div className={styles.cardFooter}>
+        <div>Short answer:</div>
+        <p className={styles.shortAnswer}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+          facilis quis ipsum, cum eligendi animi.
+        </p>
+      </div>
+
+      <Button>View</Button>
+    </div>
+  )
 }
