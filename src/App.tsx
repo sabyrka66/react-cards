d5c88206644cd43d8aa8483cf4f3f6e39f9f472a
Sabyrka <sabyrka66@gmail.com>
2026-02-02 16:37:05 +0500

    feat: create home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MainLayout } from './components/MainLayout'
+import { HomePage } from './pages/HomePage'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element="main" />
+          <Route path="/" element={<HomePage />} />
           <Route path="/add-question" element="add question" />
         </Route>
       </Routes>
